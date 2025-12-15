ac341a3ce49588d0f8b4a61faa49e0b97ab67d35
Gaurav Budhwani <gaurav.budhwani@iitgn.ac.in>
2025-12-15 20:18:59 +0530

    Fix: Update Blog link to use HashRouter path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         { name: 'About', path: '/#about' },
         { name: 'Projects', path: '/#projects' },
         { name: 'Skills', path: '/#skills' },
-        { name: 'Blog', path: '/blog' },
+        { name: 'Blog', path: '/#/blog' },
         { name: 'Contact', path: '/#contact' }
     ];
 
